e698a79283d97fbf3ef77f9cda7798f4ff15654b
921starfish <yuya.921.hoshino@gmail.com>
2021-02-11 20:13:02 +0900

    Discord.net公式の最も簡単なサンプルが動いた

--- MyDiscordBot/Program.cs
+++ MyDiscordBot/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 
 namespace MyDiscordBot
 {
     class Program
     {
+        private readonly DiscordSocketClient _client;
+        private readonly IConfigurationRoot _configuration;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public Program()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += LogAsync;
+            _client.Ready += ReadyAsync;
+            _client.MessageReceived += MessageReceivedAsync;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(path: "appsettings.json");
+            _configuration = builder.Build();
+        }
+
+        public async Task MainAsync()
+        {
+            await _client.LoginAsync(TokenType.Bot, _configuration["token"]);
+            await _client.StartAsync();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"{_client.CurrentUser} is connected!");
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;
+
+            if (message.Content == "!ping")
+                await message.Channel.SendMessageAsync("pong!");
         }
     }
 }
