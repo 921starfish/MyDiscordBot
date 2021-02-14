a22dadead25450d94bf49a9cdb6c56796cdf78dd
921starfish <yuya.921.hoshino@gmail.com>
2021-02-14 17:20:36 +0900

    ワードウルフの投票機能以外は大体できた。

--- MyDiscordBot/Modules/WordwolfModule.cs
+++ MyDiscordBot/Modules/WordwolfModule.cs
@@ -1,8 +1,10 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Microsoft.Extensions.Configuration;
 using MyDiscordBot.Services;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,15 +13,43 @@ namespace MyDiscordBot.Modules
     public class WordwolfModule : ModuleBase<SocketCommandContext>
     {
         // ↓DI
-        public WordwolfService wordwolfService { get; set; }
+        public WordwolfService WordwolfService { get; set; }
         public IConfiguration Configuration { get; set; }
-        public MyFirstBotDbContext db { get; set; }
+        public MyFirstBotDbContext DB { get; set; }
         // ↑DI
 
         [Command("wordwolf")]
-        public Task Wordwolf()
+        public async Task Wordwolf(params IUser[] users)
         {
-            return ReplyAsync("wordwolf");
+            var status = await WordwolfService.GetStatus();
+            if (status.IsNowPlaying)
+            {
+                await ReplyAsync("テーマは「" + status.Theme.A + "」と「" + status.Theme.B + "」でした。");
+                await ReplyAsync("人狼は" + status.Wolf.Username + "でした。");
+                await WordwolfService.EndGame();
+            }
+            else
+            {
+                var userList = users.Distinct().ToList();
+                if (userList.Count < 1)// 多分3が適正
+                {
+                    await ReplyAsync("ワードウルフの最低プレイ人数は3人です！");
+                    return;
+                }
+                var triple = await WordwolfService.StartGame(userList);
+
+                await Task.WhenAll(userList.Select(async user =>
+                {
+                    if (user.Id == triple.Item1.Id)
+                    {
+                        await user.SendMessageAsync(triple.Item2);
+                    }
+                    else
+                    {
+                        await user.SendMessageAsync(triple.Item3);
+                    }
+                }));
+            }
         }
     }
 }
--- MyDiscordBot/Services/WordwolfService.cs
+++ MyDiscordBot/Services/WordwolfService.cs
@@ -1,11 +1,61 @@
-﻿using System.IO;
+﻿using Discord;
+using MyDiscordBot.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyDiscordBot.Services
 {
     public class WordwolfService
     {
+        private Status status { get; set; }
+        public MyFirstBotDbContext DB { get; set; }
+
+        public WordwolfService(MyFirstBotDbContext _db)
+        {
+            DB = _db;
+            status = new Status()
+            {
+                IsNowPlaying = false,
+                Wolf = null,
+                Theme = null
+            };
+        }
+
+        public async Task<(IUser, string, string)> StartGame(List<IUser> users)
+        {
+            status.IsNowPlaying = true;
+
+            var random = new Random();
+            status.Wolf = users[random.Next(users.Count - 1)];
+
+            var allThemes = DB.WordwolfThemes.ToList();
+            status.Theme = allThemes[random.Next(allThemes.Count - 1)];
+            var (jinroTheme, villagerTheme) = random.Next(1) == 1 ? (status.Theme.A, status.Theme.B) : (status.Theme.B, status.Theme.A);
+            return (status.Wolf, jinroTheme, villagerTheme);
+        }
+
+        public async Task EndGame()
+        {
+            status.IsNowPlaying = false;
+            status.Wolf = null;
+            status.Theme = null;
+        }
+
+        public async Task<Status> GetStatus()
+        {
+            return status;
+        }
+
+        public class Status
+        {
+            public bool IsNowPlaying { get; set; }
+            public IUser Wolf { get; set; }
+            public WordwolfTheme Theme { get; set; }
+        }
 
     }
 }
